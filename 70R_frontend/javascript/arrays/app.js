7e2cb0cbb108f3c1ac5e48e613d74db6e4f132e0
SHIVAPRANEETH <shivapranith31122003@gmail.com>
2025-12-07 17:19:20 +0530

    javascript arrays

---
+++ 70R_frontend/javascript/arrays/app.js
@@ -0,0 +1,30 @@
+let data=['prabhas','ram','charan','pawan','mahesh']
+
+let newdata='vijay'
+// console.log(data)
+// console.log(data.length)
+// console.log(data.indexOf('prabhas'))   //give index value and if suppose the element is not their it will give -1
+// console.log(data.push('vdk'))   // add the last
+// console.log(data)
+// console.log(data.pop())       // remove last element
+// // console.log(data)
+// console.log(data.unshift('alluarjun'))   // it will add at the beginning
+// // console.log(data)
+// console.log(data.shift())  // it will remove the first element
+// console.log(data)
+// console.log(data.sort())  // it will sort with the alphabets
+// console.log(data.reverse())  
+// console.log(data.join())    // it will join the elements into string method
+// console.log(data.concat(newdata))  // it will add the element at last
+
+// console.log(data.includes('nani'))   // it will check the element is their or not
+// console.log(data.includes('ram'))
+
+// let data1="this is javascript"
+// console.log(data1.split())      // it will convert array to string
+
+// console.log(data.slice(0,3))
+// console.log(data.splice(0,4,))
+console.log(data.splice(1
+    ,3,'nani'))  // it will add the element in second position and remove three elements
+console.log(data)
